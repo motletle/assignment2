66315d08a8d934c7d10df4795b9fbe522dc12680
motletle <katisomokoena618@gmail.com>
2024-11-07 01:29:08 +0200

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            
+           <img src="wingsjpg.jpg"  alt="ENJOY" style={{ width: '100%', height: 'auto' }} />
+           <h1>VISIT ONCE! COME EVERYDAY!</h1>
+            
+        </div>
+    );
+};
+
+export default Home;
